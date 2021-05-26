5a6d7ce712cc38d3f6455a1b921fbab34d1c8463
Marcelo <zedlavolecram@gmail.com>
2021-05-26 19:01:19 -0300

    added more units to FConvertProvider

--- src/Providers/FConverterProvider.php
+++ src/Providers/FConverterProvider.php
@@ -9,16 +9,20 @@ class FConverterProvider extends Provider
 {
 	const LBF_PER_KGF = 2.2046231;
 
-	const N_PER_KGF   = 9.81;
+	const N_PER_KGF   = 9.806652048217;
 
 	public function register()
 	{
 		$this->container->bind('fconvert',
 			function () {
 				return new UnitsConverter([
-					'N'   => self::N_PER_KGF,
-					'lbf' => self::LBF_PER_KGF,
-					'gf'  => 1000.0,
+					'N'      => self::N_PER_KGF,
+					'lbf'    => self::LBF_PER_KGF,
+					'gf'     => 1000.0,
+					'ouncef' => 35.2739694,
+					'poundf' => self::LBF_PER_KGF,
+					'dyne'   => self::N_PER_KGF * 1e5,
+					'sthene' => self::N_PER_KGF * 1e-3,
 				]);
 			});
 	}
