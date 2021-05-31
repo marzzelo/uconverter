b7909511c03ca7d3c70ba0aa7ab2bc3746f5ff3d
Marcelo <zedlavolecram@gmail.com>
2021-05-31 19:15:28 -0300

    Unit Tests refactorization

--- tests/UnitsConverterTest.php
+++ tests/UnitsConverterTest.php
@@ -8,12 +8,26 @@ use Marzzelo\Uconverter\Exceptions\UnitsConverterException;
 
 class UnitsConverterTest extends TestCase
 {
+	const THE_KGF_VALUE_MUST_BE_NUMERIC           = 'The [kgf] value must be numeric';
+
+	const THE_KGF_VALUE_MUST_BE_GREATER_THAN_ZERO = 'The [kgf] value must be greater than zero';
+
+	const UNKNOWN_UNIT_Y                          = 'Unknown Unit: y';
+
+	const UNKNOWN_BASE_UNIT_Y                     = 'Unknown Base Unit: y';
+
+	const NEWTONS_PER_KGF                         = 9.806652048217;
+
+	const CM_PER_INCH                             = 2.5400000025908;
+
+	const LBF_PER_KGF                                       = 2.2046;
+
 	/** @test */
 	public function assigning_alpha_value_throws_exception()
 	{
 		$converter = new UnitsConverter;
-		$this->expectException('Marzzelo\Uconverter\Exceptions\UnitsConverterException');
-		$this->expectExceptionMessage('The [kgf] value must be numeric');
+		$this->expectException(UnitsConverterException::class);
+		$this->expectExceptionMessage(self::THE_KGF_VALUE_MUST_BE_NUMERIC);
 
 		$converter->kgf = 'x';
 	}
@@ -22,8 +36,8 @@ class UnitsConverterTest extends TestCase
 	public function assigning_zero_value_throws_exception()
 	{
 		$converter = new UnitsConverter;
-		$this->expectException('Marzzelo\Uconverter\Exceptions\UnitsConverterException');
-		$this->expectExceptionMessage('The [kgf] value must be greater than zero');
+		$this->expectException(UnitsConverterException::class);
+		$this->expectExceptionMessage(self::THE_KGF_VALUE_MUST_BE_GREATER_THAN_ZERO);
 
 		$converter->kgf = 0;
 	}
@@ -31,8 +45,8 @@ class UnitsConverterTest extends TestCase
 	/** @test */
 	public function assigning_zero_value_in_the_constructor_throws_exception()
 	{
-		$this->expectException('Marzzelo\Uconverter\Exceptions\UnitsConverterException');
-		$this->expectExceptionMessage('The [kgf] value must be greater than zero');
+		$this->expectException(UnitsConverterException::class);
+		$this->expectExceptionMessage(self::THE_KGF_VALUE_MUST_BE_GREATER_THAN_ZERO);
 
 		$converter = new UnitsConverter([
 			'kgf' => 0,
@@ -44,17 +58,17 @@ class UnitsConverterTest extends TestCase
 	{
 		$converter = new UnitsConverter([
 			'kgf' => 1,
-			'N'   => 9.8066,
+			'N'   => self::NEWTONS_PER_KGF,
 		]);
 
 		$this->assertEquals(1, $converter->kgf);
 
-		$converter->lbf = 2.2046;
-		$this->assertEquals(2.2046, $converter->lbf);
-		$this->assertEquals(1.0, $converter->kgf(2.2046, 'lbf'));
+		$converter->lbf = self::LBF_PER_KGF;
+		$this->assertEquals(self::LBF_PER_KGF, $converter->lbf);
+		$this->assertEquals(1.0, $converter->kgf(self::LBF_PER_KGF, 'lbf'));
 
 		$this->assertEquals(1000, $converter->lbf(1, 'klbf'));
-		$this->assertEquals(1000.0, $converter->kgf(2.2046, 'klbf'));
+		$this->assertEquals(1000.0, $converter->kgf(self::LBF_PER_KGF, 'klbf'));
 	}
 
 	/** @test */
@@ -62,11 +76,11 @@ class UnitsConverterTest extends TestCase
 	{
 		$converter = new UnitsConverter([
 			'gf' => 1000.0,
-			'N'  => 9.8066,
+			'N'  => self::NEWTONS_PER_KGF,
 		]);
 
-		$this->assertEquals(98.066, $converter->N(10.0, 'kgf'));
-		$this->assertEquals(10.0 / 9.8066, $converter->kgf(10.0, 'N'));
+		$this->assertEquals(10 * self::NEWTONS_PER_KGF, $converter->N(10.0, 'kgf'));
+		$this->assertEquals(10.0 / self::NEWTONS_PER_KGF, $converter->kgf(10.0, 'N'));
 	}
 
 	/** @test */
@@ -74,15 +88,15 @@ class UnitsConverterTest extends TestCase
 	{
 		$converter = new UnitsConverter([
 			'gf' => 1000.0,
-			'N'  => 9.8066,
+			'N'  => self::NEWTONS_PER_KGF,
 		]);
 
-		$this->assertEquals(10 / 9.8066, $converter->kgf(1.0, 'daN'));     // derived to base
-		$this->assertEquals(9.8066 / 10, $converter->daN(1.0, 'kgf'));     // base to derived
-		$this->assertEquals(100.0, $converter->daN(1.0, 'kN'));            // derived to derived
-		$this->assertEquals(1.0e+18, $converter->uN(1.0, 'TN'));           // big number
-		$converter->lbf = 2.2046;                                          // dyn. added unit
-		$this->assertEquals(1.0, 1.0e+18 * $converter->Tlbf(1.0, 'ulbf')); // small number
+		$this->assertEquals(10 / self::NEWTONS_PER_KGF, $converter->kgf(1.0, 'daN'));     // derived to base
+		$this->assertEquals(self::NEWTONS_PER_KGF / 10, $converter->daN(1.0, 'kgf'));     // base to derived
+		$this->assertEquals(100.0, $converter->daN(1.0, 'kN'));                           // derived to derived
+		$this->assertEquals(1.0e+18, $converter->uN(1.0, 'TN'));                          // big number
+		$converter->lbf = self::LBF_PER_KGF;                                                        // dyn. added unit
+		$this->assertEquals(1.0, 1.0e+18 * $converter->Tlbf(1.0, 'ulbf'));                // small number
 	}
 
 	/** @test */
@@ -92,7 +106,7 @@ class UnitsConverterTest extends TestCase
 			'x' => 1.0,
 		]);
 
-		$this->expectExceptionMessage('Unknown Unit: y');
+		$this->expectExceptionMessage(self::UNKNOWN_UNIT_Y);
 		$converter->x(1.0, 'y');   // unknown FROM
 	}
 
@@ -104,7 +118,7 @@ class UnitsConverterTest extends TestCase
 		]);
 
 		$this->expectException(UnitsConverterException::class);
-		$this->expectExceptionMessage("Unknown Unit: y");
+		$this->expectExceptionMessage(self::UNKNOWN_UNIT_Y);
 		$converter->y(1.0, 'x');   // Unknown TO
 	}
 
@@ -116,7 +130,7 @@ class UnitsConverterTest extends TestCase
 		]);
 
 		$this->expectException(UnitsConverterException::class);
-		$this->expectExceptionMessage('Unknown Base Unit: y');
+		$this->expectExceptionMessage(self::UNKNOWN_BASE_UNIT_Y);
 		$converter->ky(1.0, 'x');   // Unknown TO
 	}
 
@@ -128,9 +142,9 @@ class UnitsConverterTest extends TestCase
 		$pconversor = UnitsConverter::getConverter('pressure');
 		$lconversor = UnitsConverter::getConverter('length');
 
-		$this->assertEquals(9.806652048217, $fconversor->N(1, 'kgf'));
+		$this->assertEquals(self::NEWTONS_PER_KGF, $fconversor->N(1, 'kgf'));
 		$this->assertEquals(1e6, $pconversor->Pa(1, 'MPa'));
-		$this->assertEquals(2.5400000025908, $lconversor->cm(1, 'in'));
+		$this->assertEquals(self::CM_PER_INCH, $lconversor->cm(1, 'in'));
 	}
 
 }
